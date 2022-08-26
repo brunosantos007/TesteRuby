15aa71d9b66e6dcfb57f439dbd9ed5a984d6a0e7
Bruno Santos <brunogsantoss@outlook.com>
2022-08-26 17:34:37 -0300

    Novos exercicios feitos

---
+++ calc.rb
@@ -0,0 +1,8 @@
+print "Digite o primeiro numero inteiro: "
+# .to_i transforma a string em um numero inteiro
+number1 = gets.chomp.to_i
+print "Digite o segundo numero inteiro: "
+number2 = gets.chomp.to_i
+adicao = number1 + number2
+
+puts "O resultado da soma entre os numeros será: #{adicao}"
---
+++ retornaResultado.rb
@@ -0,0 +1,9 @@
+#  saida de dados
+print   "Digite seu nome: "
+
+# Recebendo uma entrada do teclado
+name = gets.chomp
+
+# saida utilizando puts
+# use códigos ruby dentro de uma string com #{code}
+puts "Ola #{name}"
