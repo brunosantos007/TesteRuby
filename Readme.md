d0c7936d3187ef7fbe60499d0e55e41949e9eba5
Bruno Santos <brunogsantoss@outlook.com>
2022-08-30 12:43:07 -0300

    Testes de ruby

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+    <div style="border: 2 px black;background-color: brown;">
+        <h1 style="text-align: center; color: blue;">Ola mundo</h1>
+    </div>
+    
--- exercicios/exercicio5.rb
+++ exercicios/exercicio5.rb
@@ -1,2 +1,8 @@
-arr_multi = {}
+numbers = {"A" => 10,
+            "B" => 30,
+            "C" => 20,
+            "D" => 25,
+            "E" => 15}
+
+            puts numbers.values.max
 
